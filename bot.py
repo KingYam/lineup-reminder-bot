6c1fcd65ccf800d3c9d2568e3d1025263a90c95b
Will Foley <william.w.foley@gmail.com>
2018-08-05 23:09:48 -0400

    updated gitignore; commented out api call to message for testing

--- bot.py
+++ bot.py
@@ -14,21 +14,22 @@ slack_token = os.getenv("SLACK_BOT_TOKEN")
 sc = SlackClient(slack_token)
 
 # Get all users:
-full_users_list = json.loads(json.dumps(sc.api_call("users.list")))
+full_users_list = json.loads(json.dumps(sc.api_call("users.list")))["members"]
 
 # Only want to message users in #fantasyfootball
 channels_list_return = json.loads(json.dumps(sc.api_call("channels.list",exclude_archived="true")))["channels"]
 members_in_ff_channel = []
 
 for channel in channels_list_return:
-	# print channel["name"]
 	if (channel["name"] == "fantasyfootball"):
 		for member_id in channel["members"]:
 			members_in_ff_channel.append(member_id)
 
 
-
-print full_users_list["members"][0]["name"]
+for user in full_users_list:
+	if (not user["is_bot"] and not user["deleted"] and not user["id"] == "USLACKBOT" and user["id"] in members_in_ff_channel):
+		print user["id"]
+		print user["name"]
 
 # List of messages - chosen at random
 possible_messages = ["Hey! This is your reminder to check on your lineup before games start today. Best of luck!",
@@ -37,9 +38,9 @@ possible_messages = ["Hey! This is your reminder to check on your lineup before 
 
 chosen_message = randint(0,len(possible_messages) - 1)
 
-sc.api_call(
-  "chat.postMessage",
-  channel="@wfoley",
-  text=possible_messages[chosen_message],
-  as_user="true"
-)
+# sc.api_call(
+#   "chat.postMessage",
+#   channel="@wfoley",
+#   text=possible_messages[chosen_message],
+#   as_user="true"
+# )
