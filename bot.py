32dfe7b442ddfcd6ed17466b51d2acdc630eeed9
Will Foley <william.w.foley@gmail.com>
2018-08-05 23:15:02 -0400

    encapsulated code in functions

--- bot.py
+++ bot.py
@@ -13,34 +13,44 @@ load_dotenv(dotenv_path)
 slack_token = os.getenv("SLACK_BOT_TOKEN")
 sc = SlackClient(slack_token)
 
-# Get all users:
-full_users_list = json.loads(json.dumps(sc.api_call("users.list")))["members"]
 
-# Only want to message users in #fantasyfootball
-channels_list_return = json.loads(json.dumps(sc.api_call("channels.list",exclude_archived="true")))["channels"]
-members_in_ff_channel = []
+def get_user_list():
+	# Get all users:
+	full_users_list = json.loads(json.dumps(sc.api_call("users.list")))["members"]
 
-for channel in channels_list_return:
-	if (channel["name"] == "fantasyfootball"):
-		for member_id in channel["members"]:
-			members_in_ff_channel.append(member_id)
+	# Only want to message users in #fantasyfootball
+	channels_list_return = json.loads(json.dumps(sc.api_call("channels.list",exclude_archived="true")))["channels"]
+	members_in_ff_channel = []
+
+	for channel in channels_list_return:
+		if (channel["name"] == "fantasyfootball"):
+			for member_id in channel["members"]:
+				members_in_ff_channel.append(member_id)
+
+	for user in full_users_list:
+		if (not user["is_bot"] and not user["deleted"] and not user["id"] == "USLACKBOT" and user["id"] in members_in_ff_channel):
+			print user["id"]
+			print user["name"]
+
+def message_reminder(user_list):
+
+	# List of messages - chosen at random
+	possible_messages = ["Hey! This is your reminder to check on your lineup before games start today. Best of luck!",
+	"*YO* - this is your reminder: check your roster! No one wants to see your opponent win because you didn't replace that TE you have on BYE.",
+	"something NOT useful"]
+
+	chosen_message = randint(0,len(possible_messages) - 1)
+
+	# message users
+	sc.api_call(
+	  "chat.postMessage",
+	  channel="@wfoley",
+	  text=possible_messages[chosen_message],
+	  as_user="true"
+	)
+user_list = get_user_list()
+# message_reminder(user_list)
 
 
-for user in full_users_list:
-	if (not user["is_bot"] and not user["deleted"] and not user["id"] == "USLACKBOT" and user["id"] in members_in_ff_channel):
-		print user["id"]
-		print user["name"]
 
-# List of messages - chosen at random
-possible_messages = ["Hey! This is your reminder to check on your lineup before games start today. Best of luck!",
-"*YO* - this is your reminder: check your roster! No one wants to see your opponent win because you didn't replace that TE you have on BYE.",
-"something NOT useful"]
 
-chosen_message = randint(0,len(possible_messages) - 1)
-
-# sc.api_call(
-#   "chat.postMessage",
-#   channel="@wfoley",
-#   text=possible_messages[chosen_message],
-#   as_user="true"
-# )
